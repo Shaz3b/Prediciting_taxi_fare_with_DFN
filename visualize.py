2dc23a50e13d93fce0de4d6f8eb40fa227fdd705
Shaz3b <Shazebhussaini1994@gmail.com>
2019-12-17 14:40:03 +0530

    add nyc location

--- visualize.py
+++ visualize.py
@@ -3,7 +3,6 @@ import matplotlib
 import pandas as pd
 
 df = pd.read_csv('NYC_taxi.csv', parse_dates=['pickup_datetime'], nrows=500000)
-print(df.head()
 
 # nyc longtitude
 nyc_min_longtitude = -74.05
@@ -15,3 +14,4 @@ nyc_min_latitude = 40.85
 
 df2 = df.copy(deep=True)
 
+print(df2.head())
